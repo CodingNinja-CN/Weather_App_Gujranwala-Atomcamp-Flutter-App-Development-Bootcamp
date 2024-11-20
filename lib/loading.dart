ce0c2c3b1ee43f4fc6586b8499fa602b8add23f0
Coding Ninja <cn.codingninja@gmail.com>
2024-11-20 21:21:19 +0500

    API call and Importing packages

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,33 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
   const Loading({super.key});
 
   @override
+  State<Loading> createState() => _LoadingState();
+}
+
+const LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.high,
+  distanceFilter: 100,
+);
+
+const apiKey = '269ebeab186884c9c33a027fdfa761c7';
+
+Future<void> getLocation() async {
+  PermissionStatus permission = await Permission.location.request();
+
+  if (permission == PermissionStatus.granted) {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+
+      print("user's location: ${position.latitude}, ${position.longitude}");
+    } catch (e) {
+      print("Error fetching the location: $e");
+    }
+  } else if (permission == PermissionStatus.denied) {
+    print("Location permission denied");
+  } else if (permission == PermissionStatus.permanentlyDenied) {
+    print('Location permission permanently denied');
+    openAppSettings();
+  }
+}
+
+void getData() async {
+  final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=$apiKey'));
+
+  if (response.statusCode == 200) {
+    print('Data loaded successfully : ${response.body}');
+  } else {
+    print('Failed to laod data: ${response.statusCode} - ${response.body}');
+  }
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            "Loading Screen",
-            style: TextStyle(color: Colors.white, fontSize: 25),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.black,
-        ),
-        body: Center(
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.blue,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            ),
-            onPressed: () {},
-            child: Text('Get Location',style: TextStyle(fontSize: 20),),
-          ),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 214, 214, 214),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text(
+          'Loading screen',
+          style: TextStyle(color: Colors.white, fontSize: 25),
         ),
       ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                getData();
+              },
+              child: const Text(
+                'Get Location',
+                style: TextStyle(color: Colors.black),
+              ))),
     );
   }
 }
